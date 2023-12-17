cab87ca335ebdbcc99815feffe6e9701101d62b0
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-12-17 19:19:37 +0530

    final touch

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
+import { Fragment } from "react";
+
 function NewsPage() {
-    return(
-        <h1>Home Page</h1>
-    )
+    <Fragment>
+        <h1>The News Page</h1>
+        <ul>
+            <li>
+                <Link href="/news/nextjs-is-a-great-framework">
+                    NextJs is a framework
+                </Link>
+            </li>
+            <li>
+                Something else
+            </li>
+        </ul>
+    </Fragment>
 }
 
 
